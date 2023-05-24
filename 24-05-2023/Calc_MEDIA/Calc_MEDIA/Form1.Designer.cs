d50aa07c7bf5f1cabb3fefd3c27e617db5fc6d12
Iago F <n3rdydzn@hotmail.com>
2023-05-24 12:04:52 -0300

    24/05/2023 2

---
+++ 24-05-2023/Calc_MEDIA/Calc_MEDIA/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Calc_MEDIA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.TextBox();
+            this.lblN1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblN2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblN3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(138, 304);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(293, 31);
+            this.lblResultado.TabIndex = 0;
+            this.lblResultado.Text = "A media do NOME é: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nome do Aluno:";
+            // 
+            // lblNome
+            // 
+            this.lblNome.Location = new System.Drawing.Point(174, 62);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(206, 20);
+            this.lblNome.TabIndex = 3;
+            // 
+            // lblN1
+            // 
+            this.lblN1.Location = new System.Drawing.Point(174, 101);
+            this.lblN1.Name = "lblN1";
+            this.lblN1.Size = new System.Drawing.Size(206, 20);
+            this.lblN1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(171, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nota 1:";
+            // 
+            // lblN2
+            // 
+            this.lblN2.Location = new System.Drawing.Point(174, 149);
+            this.lblN2.Name = "lblN2";
+            this.lblN2.Size = new System.Drawing.Size(206, 20);
+            this.lblN2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(171, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Nota 2:";
+            // 
+            // lblN3
+            // 
+            this.lblN3.Location = new System.Drawing.Point(174, 194);
+            this.lblN3.Name = "lblN3";
+            this.lblN3.Size = new System.Drawing.Size(206, 20);
+            this.lblN3.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(171, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Nota 3:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 408);
+            this.Controls.Add(this.lblN3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblN2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblN1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblResultado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lblNome;
+        private System.Windows.Forms.TextBox lblN1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox lblN2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox lblN3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ 24-05-2023/Calc_MEDIA/Calc_MEDIA/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc_MEDIA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblResultado.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(lblN1.Text);
+            int n2 = Convert.ToInt32(lblN2.Text);
+            int n3 = Convert.ToInt32(lblN3.Text);
+            int media = (n1+n2+n3)/3;
+            lblResultado.Text = $"A media do {lblNome.Text} é: {media}";
+            lblResultado.Visible=true;
+        }
+    }
+}
